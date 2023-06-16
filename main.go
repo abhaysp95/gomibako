ab1b20e67cf97822f48910192281a751c86ec67f
abhay <abhaysp95@protonmail.com>
2023-06-16 18:14:06 +0530

    feat: added some more simple handlers

--- main.go
+++ main.go
@@ -11,12 +11,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from gomibako"));
+	w.Write([]byte("Hello from gomibako"))
+}
+
+// handler to showing individual gomi
+func showGomi(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("handler for showing gomi"))
+}
+
+// handler to create new gomi
+func createGomi(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not supported", 405)
+		return
+	}
+
+	w.Write([]byte("create gomi"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/gomi", showGomi)
+	mux.HandleFunc("/gomi/create", createGomi)
 
 	log.Println("Starting at :4000")
 	err := http.ListenAndServe(":4000", mux)
