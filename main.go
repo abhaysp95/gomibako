09be8036adc24735afb6c4b9dcb6c4bcc49cbfd9
abhay <abhaysp95@protonmail.com>
2023-06-16 18:34:23 +0530

    feat: query filtering

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // handler to showing individual gomi
 func showGomi(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("handler for showing gomi"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show gomi with ID: %d...", id)
 }
 
 // handler to create new gomi
