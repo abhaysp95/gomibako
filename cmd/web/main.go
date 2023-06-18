0736c291fae6388a433789f81ae2683f7b71962b
abhay <abhaysp95@protonmail.com>
2023-06-18 23:09:54 +0530

    feat: add initial config from .env file
        also added flag for dsn

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // holds application-wide dependencies
@@ -14,25 +19,66 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "provide addr to run server")
-	flag.Parse()
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stdout, "ERR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	app := &application { infoLog, errLog }
+	err := godotenv.Load()
+	if err != nil {
+		app.errLog.Println("Error loading .env file")
+	}
+
+	var addr string
+	var dsn string
+
+	flag.StringVar(&addr, "addr", "", "Provide addr to run server")
+	flag.StringVar(&dsn, "dsn", "", "Mariadb data source name")
+	flag.Parse()
+
+	if (addr == "") {
+		addr = os.Getenv("ADDR")
+	}
+	if (dsn == "") {
+		dsn = fmt.Sprintf("%s:%s@/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DB"))
+	}
+
+	if (addr == "" || dsn == "") {
+		app.errLog.Fatal("Provide initial server and db configuration to run application. See -h for more info")
+	}
+
+	db, err := app.openDB(dsn)
+	if err != nil {
+		app.errLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	mux := app.routes()
 
 	srv := http.Server {
-		Addr: *addr,
+		Addr: addr,
 		Handler: mux,
 		ErrorLog: errLog,
 	}
 
-	infoLog.Println("Starting at ", *addr)
-	err := srv.ListenAndServe()
+	infoLog.Println("Starting at ", addr)
+	err = srv.ListenAndServe()
 	if err != nil {
 		errLog.Fatal(err)
 	}
+}
+
+func (app *application) openDB(dsn string) (*sql.DB, error)  {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	app.infoLog.Println("DB connection successful")
+
+	return db, nil
 }
