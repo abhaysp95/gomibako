6ad7ffe591ab4b22bbad17d9e32947d67aed20d2
abhay <abhaysp95@protonmail.com>
2023-07-15 17:28:54 +0530

    feat: add basic auth buttons in ui and prepare schema for users

--- pkg/gomi.sql
+++ pkg/gomi.sql
@@ -8,6 +8,7 @@ create table gomi (
 
 create index gomi_idx_created on gomi(created);
 
+-- dummy data
 INSERT INTO gomi (title, content, created, expires) VALUES (
     'An old silent pond',
     'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.',
@@ -28,3 +29,16 @@ INSERT INTO gomi (title, content, created, expires) VALUES (
 	UTC_TIMESTAMP(),
 	DATE_ADD(UTC_TIMESTAMP(), INTERVAL 7 DAY)
 );
+
+create table users (
+	id integer primary key not null auto_increment,
+	name varchar(100) not null,
+	email varchar(255) not null,
+	passwd char(60) not null,
+	created datetime not null
+);
+
+alter table users add constraint users_unq_email unique(email);
+
+-- to check for a constraint in a table you can use following query
+select constraint_name from information_schema.table_constraints where table_name='users' and constraint_type='unique';
