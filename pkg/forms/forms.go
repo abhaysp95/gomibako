6965516a39c74829be1621e058fc37842bda8cd1
abhay <abhaysp95@protonmail.com>
2023-07-16 17:23:17 +0530

    feat: add new validation check functions

--- pkg/forms/forms.go
+++ pkg/forms/forms.go
@@ -3,9 +3,12 @@ package forms
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRx = regexp.MustCompile("^[a-zA-Z0-9.\\/+-_~]+@([a-zA-Z0-9]+.)+[a-zA-Z]{2,4}$")
 
 // holds form data and errMap for any validation message related to form data
 type Form struct {
@@ -34,8 +37,31 @@ func (f *Form) Required(fields ...string) {
 // validation for "maximum length". In case of failure, appropriate error is added to errMap
 func (f *Form) MaxLength(field string, d int) {
 	value := f.Get(field)
+	if value == "" {
+		return
+	}
 	if utf8.RuneCountInString(value) > d {
 		f.ErrMap.Add(field, fmt.Sprintf("%s's length can't be more than %d", field, d))
+	}
+}
+
+func (f *Form) MinLength(field string, d int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if utf8.RuneCountInString(value) < d {
+		f.ErrMap.Add(field, fmt.Sprintf("%s's length should be more than %d", field, d))
+	}
+}
+
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if !pattern.MatchString(value) {
+		f.ErrMap.Add(field, fmt.Sprintf("%s is not valid", field))
 	}
 }
 
